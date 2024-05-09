d4abe38d59150ebb92119c94cfb39494695da310
Saksham0205 <saksham252003@gmail.com>
2024-05-09 12:15:12 +0530

    container properties

--- lib/widgets/container_sized_box.dart
+++ lib/widgets/container_sized_box.dart
@@ -7,10 +7,34 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Container abd Sized Box"),
+        title: const Text("Container and Sized Box"),
+        backgroundColor: Colors.green,
+      ),
+      body: Center(
+        child: Container(
+          width: 100,
+          height: 100,
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                blurRadius: 20,
+                spreadRadius: 20
 
+              )
+            ],
+            color: Colors.purple,
+                borderRadius: BorderRadius.only(topLeft: Radius.circular(20),)
+          ),
+          child: Center(
+            child: Text(
+              "Hello Ji!"
+            ),
+          ),
+        ),
 
       ),
+
+
     );
   }
 }
