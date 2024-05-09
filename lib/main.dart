320d6d5debdafae24f91e1acbca7a2a4e1ee791d
Saksham0205 <saksham252003@gmail.com>
2024-05-09 12:07:20 +0530

    container and sized box

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
-import
+import 'package:flutter/material.dart';
+import 'package:flutter_30_days/widgets/container_sized_box.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Colors.green,
+        brightness: Brightness.dark,
+      ),
+      home: HomePage(),
+    );
+  }
+}
