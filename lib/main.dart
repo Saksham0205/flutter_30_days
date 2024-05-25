e02ed47605570c1d4a8de8ef66cdae92b177a77c
Saksham0205 <saksham252003@gmail.com>
2024-05-25 22:22:26 +0530

    Image_Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_30_days/widgets/drawer.dart';
+import 'package:flutter_30_days/widgets/image_widget.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.green,
         brightness: Brightness.dark,
       ),
-      home: DrawerWidget(),
+      home: ImageWidget(),
     );
   }
 }
---
+++ lib/widgets/image_widget.dart
@@ -0,0 +1,28 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:flutter/material.dart';
+
+class ImageWidget extends StatelessWidget {
+  const ImageWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Image Widget"),
+      ),
+      body: Center(
+        child: Container(
+          height: 200,
+          width: 300,
+          child: CachedNetworkImage(
+            imageUrl: "https://wallpapercave.com/wp/wp9132427.jpg",
+            fit: BoxFit.cover,
+            placeholder: (context, url) =>
+                Center(child: CircularProgressIndicator()),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+          ),
+        ),
+      ),
+    );
+  }
+}
